3da8f92cc6f00d36caf5c6e851f9dc0d9d15b9be
dvozicnivod <radovanzivkovic@yahoo.com>
2025-05-25 20:50:31 +0200

    * Python call added

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 # Windows
-pyinstaller --onefile --name allure_viewer --add-data "temp;temp" --hidden-import tkinter allure_offline_viewer.py
+python.exe -m pyinstaller --onefile --name allure_viewer --add-data "temp;temp" --hidden-import tkinter allure_offline_viewer.py
